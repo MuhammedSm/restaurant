52a0942ba158d4e7c1040e86880bddefcc55412b
MuhammedSm <muhammedseyhmusa5@gmail.com>
2024-04-24 20:02:49 +0300

    cleaning up

--- src/pages/Booking/BookingForm.tsx
+++ src/pages/Booking/BookingForm.tsx
@@ -28,12 +28,10 @@ function BookingForm() {
   const [quantity, setQuantity] = useState<number>(1);
 
   const handleIncrement = () => {
-    // Add any additional logic you need for incrementing
     setQuantity((prevQuantity: number) => prevQuantity + 1);
   };
 
   const handleDecrement = () => {
-    // Add any additional logic you need for decrementing
     setQuantity((prevQuantity: number) => Math.max(prevQuantity - 1, 1));
   };
 
@@ -143,33 +141,7 @@ function BookingForm() {
               />
             </div>
           </div>
-          {/* <div>
-            <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">
-              Pick Your Table
-            </label>
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 16"
-                >
-                  <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                  <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                </svg>
-              </div>
-              <input
-                className="bg-gray-50 border border-gray-300  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="0"
-                type="text"
-                id="table"
-                {...register("table", { required: true })}
-                defaultValue={defaultValues.table}
-              />
-            </div>
-          </div> */}
+
           <div>
             <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">
               Date
