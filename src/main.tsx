b48c9f3e64ffc0226c38d0796f49015f4c8a2902
MuhammedSm <muhammedseyhmusa5@gmail.com>
2024-05-05 12:14:38 +0300

    hasrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./styles/globals.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "swiper/swiper-bundle.css";
 
 import App from "./App.tsx";
@@ -12,9 +12,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
